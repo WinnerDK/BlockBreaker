98d725169d030acc64900e2ced58189509d9b3e1
WinnerDK <46270948+WinnerDK@users.noreply.github.com>
2019-01-07 07:43:55 +0700

    Update code to lecture 54
    lecture 54 Distance As A Vector2

---
+++ Scripts/PaddleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+    [SerializeField] float sceneWithInUnits = 16f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float minX = 1f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * sceneWithInUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX); 
+        transform.position = paddlePos;
+    }
+}
