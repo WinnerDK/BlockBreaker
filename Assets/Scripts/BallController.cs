0510f9c3b65d5950144a091e282719f03e1b9bab
WinnerDK <46270948+WinnerDK@users.noreply.github.com>
2019-01-08 18:21:37 +0700

    Update code to section 5 lecture 61
    Get components to play audio

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    // params
+    [SerializeField] PaddleController paddle;
+
+    // game state
+    Vector2 paddleToBallVector;
+    bool isStart = false;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isStart)
+        {
+            LockBallToPaddle();
+            LauchBallOnMouseClick();
+        }
+	}
+
+    private void LockBallToPaddle()
+    {
+        var paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        var ballPos = new Vector2(paddlePos.x + paddleToBallVector.x, paddlePos.y + paddleToBallVector.y);
+        transform.position = ballPos;
+    }
+
+    private void LauchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isStart = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+        }
+    }
+}
